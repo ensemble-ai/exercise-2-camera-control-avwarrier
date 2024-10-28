bcc3d4d22559815fcd2cf28c2519f6adbdf60978
avwarrier <avwarrier@ucdavis.edu>
2024-10-27 23:19:34 -0700

    draw logic added

--- Obscura/scripts/camera_controllers/lerp_smoothing.gd
+++ Obscura/scripts/camera_controllers/lerp_smoothing.gd
@@ -13,7 +13,6 @@ func _ready() -> void:
 	
 
 func _process(delta: float) -> void:
-	draw_camera_logic = true
 	if !current:
 		return
 	
@@ -60,36 +59,34 @@ func _process(delta: float) -> void:
 
 
 func draw_logic() -> void:
-	#var mesh_instance := MeshInstance3D.new()
-	#var immediate_mesh := ImmediateMesh.new()
-	#var material := ORMMaterial3D.new()
-	#
-	#mesh_instance.mesh = immediate_mesh
-	#mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
-	#
-	#immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
-	#immediate_mesh.surface_add_vertex(Vector3(-bottom_right.x - 0.5, 0, top_left.y))
-	#immediate_mesh.surface_add_vertex(Vector3(-bottom_right.x - 0.5, 0, bottom_right.y))
-	#
-	#immediate_mesh.surface_add_vertex(Vector3(-top_left.x, 0, top_left.y))
-	#immediate_mesh.surface_add_vertex(Vector3(-top_left.x, 0, bottom_right.y))
-	#
-	#immediate_mesh.surface_add_vertex(Vector3(-top_left.x, 0, top_left.y))
-	#immediate_mesh.surface_add_vertex(Vector3(-bottom_right.x - 0.5, 0, top_left.y))
-	#
-	#immediate_mesh.surface_add_vertex(Vector3(-top_left.x, 0, bottom_right.y))
-	#immediate_mesh.surface_add_vertex(Vector3(-bottom_right.x - 0.5, 0, bottom_right.y))
-	#
-	#immediate_mesh.surface_end()
-#
-	#material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	#material.albedo_color = Color.WHITE
+	var mesh_instance := MeshInstance3D.new()
+	var immediate_mesh := ImmediateMesh.new()
+	var material := ORMMaterial3D.new()
 	
-	pass
-	#add_child(mesh_instance)
-	#mesh_instance.global_transform = Transform3D.IDENTITY
-	#mesh_instance.global_position = Vector3(global_position.x, target.global_position.y, global_position.z)
-	#
-	##mesh is freed after one update of _process
-	#await get_tree().process_frame
-	#mesh_instance.queue_free()
+	mesh_instance.mesh = immediate_mesh
+	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	
+	var left:float = -10 / 2
+	var right:float = 10 / 2
+	var top:float = -10 / 2
+	var bottom:float = 10 / 2
+	
+	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
+	immediate_mesh.surface_add_vertex(Vector3(0, 0, top))
+	immediate_mesh.surface_add_vertex(Vector3(0, 0, bottom))
+	
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, 0))
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, 0))
+	
+	immediate_mesh.surface_end()
+
+	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	material.albedo_color = Color.WHITE
+	
+	add_child(mesh_instance)
+	mesh_instance.global_transform = Transform3D.IDENTITY
+	mesh_instance.global_position = Vector3(global_position.x, target.global_position.y, global_position.z)
+	
+	#mesh is freed after one update of _process
+	await get_tree().process_frame
+	mesh_instance.queue_free()
